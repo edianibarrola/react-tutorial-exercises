d4307f6e0e9f7b7a1c771d6cfa99a2d598bd703b
edianibarrola <edianibarrola@gmail.com>
2021-07-19 14:36:45 +0000

    update

--- exercises/03.5-alert-component/index.js
+++ exercises/03.5-alert-component/index.js
@@ -6,7 +6,16 @@ import PropType from "prop-types";
  * here you have declare your Alert component and return
  * the html that bootstrap dictates for its alert component
  */
-
+export const Alert = (props) => {
+    return(
+        <div className="alert alert-danger" role="alert">
+            OMG! Something really bad has happended! 
+        </div>
+    )
+}
+Alert.propTypes = {
+    test: PropType.string
+}
 
 // here is where the alert component is being used, you don't have to edit this part,
 // but it helps you understan what properties is the component using
